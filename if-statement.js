5efbac4e2d10dd4d0533f2c11f6b2a21232ba926
DuvanCampo <duvancampo733@gmail.com>
2017-05-31 19:19:02 -0500

    Subo los documentos js

---
+++ if-statement.js
@@ -0,0 +1,10 @@
+var fruit ="orange"
+if(fruit>5)
+{
+console.log('the fruit name has five characters or less');
+}
+else
+{
+	console.log('The fruit name has more than five characters.');
+}
+
---
+++ looping-through-arrays.js
@@ -0,0 +1,3 @@
+var pets=['cat','dogs','rats'];
+pets[0]=pets[0] + 's';
+console.log(pets)
